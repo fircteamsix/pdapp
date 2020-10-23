b7fcf8dfb373104f5b9373cfd502d3fc77877e09
augusto almeida <augustalmeid@gmail.com>
2020-10-22 21:15:51 -0300

    Pagina Cadastro

--- js/auto.js
+++ js/auto.js
@@ -1,1 +1,28 @@
 M.AutoInit();
+document.addEventListener('DOMContentLoaded', function() {
+    var options = {
+        //defaultDate: new Date(1990,1,3),
+        yearRange: 120,
+        setDefaultDate: true,
+        format: 'dd/mm/yyyy',
+        i18n: {
+            today: 'Hoje',
+            clear: 'Limpar',
+            done: 'Ok',
+            cancel: 'Cancelar',
+            nextMonth: 'Próximo mês',
+            previousMonth: 'Mês anterior',
+            weekdaysAbbrev: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+            weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+            weekdays: ['Domingo', 'Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado'],
+            monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+            months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+        }
+    };
+    var elems = document.querySelectorAll('.datepicker');
+    var instance = M.Datepicker.init(elems, options);
+});
+
+$('.datepicker').pickadate({
+    labelMonthNext: 'P'
+})
---
+++ js/cadastro.js
@@ -0,0 +1,4 @@
+//Gravar no storage
+// var names = [];
+// names[0] = prompt("New member name?");
+// localStorage.setItem("names", JSON.stringify(names));
